0e148482dc53af9bd498083d6dd9fa53971d4068
tkadima <tabithakadima@gmail.com>
2024-05-02 22:51:23 -0400

    complete basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 
 interface User {
@@ -17,25 +17,51 @@ const users: User[] = [
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-
-  comments: string[] = []; 
+  comments: string[] = ['Waiting on Parts']; 
   newComment: string = '';
-  topUser: User|null = null; 
   users: User[] = users
-  selectedUser: User|null = null; 
+  showUserDropdown: boolean = false; 
+  tags: User[] = [];
+
+  @ViewChild('textarea') textarea!: ElementRef<HTMLTextAreaElement>;
 
   setUpdateComment(newValue: string){
     this.newComment = newValue; 
-    if (newValue.includes('@')){
-      const tagIndex = newValue.indexOf('@'); 
+    if (newValue.includes('@')) {
+      this.showUserDropdown = true;
+      const tagIndex = newValue.lastIndexOf('@'); 
       const tagged = newValue.slice(tagIndex + 1); 
-      this.getMatchingUsers(tagged)
+      this.users = this.getMatchingUsers(tagged);
     }
   }
 
-  getMatchingUsers(taggedUser: string) {
-    this.users = users.filter(user => user.name.includes(taggedUser)); 
-    this.topUser = this.users[0]; 
+  getMatchingUsers(taggedUser: string): User[] {
+    return users.filter(user => user.name.includes(taggedUser)); 
+  }
+
+  onSelect(user:User): void {
+    const textarea = this.textarea.nativeElement;
+    const currentText = textarea.value;
+    const cursorPosition = textarea.selectionStart;
+    // todo fix so it completes when user already types some
+    const newText = currentText.slice(0, cursorPosition) + user.name + currentText.slice(cursorPosition);
+    textarea.value = newText;
+    this.newComment = newText;
+
+    this.tags.push(user); 
+    // reset 
+    this.users = users; 
+    this.showUserDropdown = false; 
+  }
+
+  onSubmit(): void {
+    this.comments.push(this.newComment);
+    alert(`Tagged: ${this.tags.map(user => user.name).join(', ')}`)
+    this.newComment = '';
+    this.tags = []
+  }
+
+  formatComment(comment: string): string{
+    return comment.replace(/@(\w+)/g, '<strong>@$1</strong>');
   }
 }
